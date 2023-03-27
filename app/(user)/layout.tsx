a30effd87cd72385eee3dc9b1cc20c076c2692f2
Sean Tolbert <stolbert4150@gmail.com>
2023-03-27 18:17:23 -0400

    inits next project, adds sanity and blog schema template

---
+++ app/(user)/layout.tsx
@@ -0,0 +1,27 @@
+import "../../styles/globals.css";
+import Navbar from "@/components/Navbar";
+
+export const metadata = {
+  title: "Jungles of Dorado",
+  description:
+    "Exploring history and culture. Celebrating the beautiful, obscure, and forgotten.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="max-w-7xl mx-auto">
+        <Navbar />
+        {/* <Banner /> */}
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'd2fkxm41',
+    dataset: 'production'
+  }
+})
